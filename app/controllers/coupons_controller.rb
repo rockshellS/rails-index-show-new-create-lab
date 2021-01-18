96e50b24501c1af8f47b018e254d7e20bf94d363
rockshellS <spicer.rachelle@gmail.com>
2021-01-18 12:46:26 -0600

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -10,13 +10,23 @@ class CouponsController < ApplicationController
     end
 
     def new
+        @coupon  = Coupon.new
     end
-    
+
+    def create
+        @coupon = Coupon.create(coupon_params)
+        redirect_to coupon_path(@coupon)
+    end
 
 
 
 
 
+    private
+
+    def coupon_params
+        params.require(:coupon).permit(:coupon_code, :store)
+    end
 
 
 
